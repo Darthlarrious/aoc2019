363af87e9d930f5ba8d6259ac73dbe1753cf4a5e
Larry <laurentgratton@gmail.com>
2019-12-02 21:53:30 -0600

    day 2 initial commit

---
+++ day2.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const inputs = fs.readFileSync('inputs/day2inputs.txt')
+  .toString()
+  .split(',')
+
+let data = inputs.map(item => parseInt(item));
+
+let step = (data, start) => {
+  if(data[start] != 1 && data[start] != 2){
+    return data;
+  }
+  let item_one = data[start+1];
+  let item_two = data[start+2];
+  let destination = data[start+3];
+  if(data[start] == 1){
+    data[destination] = data[item_one] + data[item_two];
+  } else {
+    data[destination] = data[item_one] * data[item_two];
+  }
+
+  return step(data, start + 4);
+}
+
+console.log("Day 2 part 1:");
+console.log(step(data, 0)[0]);
+
+let noun = 0;
+let verb = 0;
+let target = 19690720;
+
+console.log("Day 2 part 2:");
+
+for(;noun<100;noun++){
+  verb = 0;
+  for(;verb<100;verb++){
+    let data = inputs.map(item => parseInt((item)));
+    data[1] = noun;
+    data[2] = verb;
+    let result = step(data, 0);
+    if(result[0] === target){
+      console.log('collision at:');
+      console.log(noun + " " + verb);
+      console.log("calculation: " + (100 * noun + verb));
+    }
+  }
+}
