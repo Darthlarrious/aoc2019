a139713846da7636daf918f73136ed3037f5fa9c
Darthlarrious <laurentgratton@gmail.com>
2019-12-13 15:57:35 -0600

    Start of P2

--- day10.js
+++ day10.js
@@ -12,6 +12,9 @@ input.map((row, y) => {
     });
 });
 
+let sameSlope = (source, destination, asteroid) => {
+    return (asteroid.y - source.y) * (destination.x - source.x) === (destination.y - source.y) * (asteroid.x - source.x);
+};
 
 let hasLOS = (asteroids, asteroidA, asteroidB) => {
   let source = asteroids[asteroidA];
@@ -22,11 +25,12 @@ let hasLOS = (asteroids, asteroidA, asteroidB) => {
       if(id === asteroidA || id === asteroidB){
           return false;
       }
-      return (roid.y - source.y) * (destination.x - source.x) === (destination.y - source.y) * (roid.x - source.x);
+      return sameSlope(source, destination, roid);
   });
 
   // find any that are between A and B
     // finds any points closer to A than B on the A->B slope
+    // AND in the same direction as B
     let visibility = onSlope.filter(asteroid => {
         let leftMost = source.x > destination.x ? destination : source;
         let rightMost = source.x > destination.x ? source : destination;
@@ -43,7 +47,11 @@ let hasLOS = (asteroids, asteroidA, asteroidB) => {
 };
 
 let findVisible = (asteroids, asteroidID) => {
-      return asteroids.filter(asteroid => {return asteroid.id === asteroidID ? false : hasLOS(asteroids, asteroidID, asteroid.id)});
+      return asteroids.filter(asteroid => {
+          return asteroid.id === asteroidID ?
+              false :
+              hasLOS(asteroids, asteroidID, asteroid.id)
+      });
 };
 
 let distances = asteroids.map(roid =>({
@@ -54,5 +62,30 @@ let distances = asteroids.map(roid =>({
 let best = distances.sort((a,b) => b.visible - a.visible)[0];
 
 console.log("Day 10 Part 1:");
-console.log(best.visible );
+console.log(best.visible);
 
+let  cartesian2Polar = (x, y) => {
+    let distance = Math.sqrt(x*x + y*y)
+    let radians = Math.atan2(y,x); //This takes y first
+    radians = radians < 0 ? radians + Math.PI * 2 : radians;
+    return {distance: distance, radians: radians}
+};
+
+let polars = asteroids.map((center, id) =>{
+    let others = asteroids.filter((c,i)=>i!==id);
+    //set origin to center and calculate distance and radians for each other asteroid
+    return others.map(other =>{
+      let translatedX = other.x - center.x;
+      let translatedY = other.y - center.y;
+
+      return {...cartesian2Polar(translatedX, translatedY), id:asteroids.indexOf(other)}
+    })
+})[best.id].sort((a,b)=>{
+    let mainSort = a.radians - b.radians;
+    if(mainSort === 0){
+        mainSort = a.distance - b.distance;
+    }
+    return mainSort;
+});
+
+console.log(polars);
